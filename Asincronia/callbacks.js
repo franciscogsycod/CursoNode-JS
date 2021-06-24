469c6585f012653e6f971f81d7f3bc8fafd212c3
franciscogsycod <francisco.gomez@sycod.com>
2021-06-24 14:14:47 -0500

    Callbacks

--- Asincronia/callbacks.js
+++ Asincronia/callbacks.js
@@ -1,5 +1,14 @@
-function soyAsincrono() {
+function soyAsincrono(miCallback) {
     console.log('Hola soy una funcion asincrona');
+    // Creando el asincronismo
+    setTimeout(function() {
+        console.log('Estoy siendo asincrono');
+        miCallback();
+    }, 3000);
 }
 
-soyAsincrono();
+console.log('Iniciando proceso ...');
+
+soyAsincrono(function() {
+    console.log('Terminando proceso ...');
+});
