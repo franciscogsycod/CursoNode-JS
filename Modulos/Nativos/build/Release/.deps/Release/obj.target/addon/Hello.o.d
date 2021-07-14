90304dc5a6db7db94eb76e06180c55cebc27c9f2
franciscogsycod <francisco.gomez@sycod.com>
2021-07-14 13:51:02 -0500

    Nativos C++

---
+++ Modulos/Nativos/build/Release/.deps/Release/obj.target/addon/Hello.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/addon/Hello.o := g++ -o Release/obj.target/addon/Hello.o ../Hello.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/sycod/.cache/node-gyp/15.14.0/include/node -I/home/sycod/.cache/node-gyp/15.14.0/src -I/home/sycod/.cache/node-gyp/15.14.0/deps/openssl/config -I/home/sycod/.cache/node-gyp/15.14.0/deps/openssl/openssl/include -I/home/sycod/.cache/node-gyp/15.14.0/deps/uv/include -I/home/sycod/.cache/node-gyp/15.14.0/deps/zlib -I/home/sycod/.cache/node-gyp/15.14.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/Hello.o.d.raw   -c
+Release/obj.target/addon/Hello.o: ../Hello.cc \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/node.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/v8.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/cppgc/common.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/v8config.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/v8-internal.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/v8-version.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/v8config.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/v8-platform.h \
+ /home/sycod/.cache/node-gyp/15.14.0/include/node/node_version.h
+../Hello.cc:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/node.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/v8.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/cppgc/common.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/v8config.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/v8-internal.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/v8-version.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/v8config.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/v8-platform.h:
+/home/sycod/.cache/node-gyp/15.14.0/include/node/node_version.h:
